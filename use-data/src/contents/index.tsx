4861c44c0d3dd8caddbfa9476d409709bfcebcee
ayakamada <sdkamada@gmail.com>
2023-06-01 17:50:29 +0900

    Update index.tsx

--- use-data/src/contents/index.tsx
+++ use-data/src/contents/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import type { IPost } from "@/types/Ipost";
 
 const IndexPage: React.FC<PageProps> = ({ pageContext }) => {
-  const { page } = pageContext as { page: { title: string } };
+  // console.log(pageContext.page);
+  const { page } = pageContext as IPost;
 
   return (
     <>
