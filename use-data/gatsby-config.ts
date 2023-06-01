31501ef7ce68c5d86fc8d20a170bbd6275ab2953
ayakamada <sdkamada@gmail.com>
2023-06-01 17:49:25 +0900

    alias使用

--- use-data/gatsby-config.ts
+++ use-data/gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     siteName: `gatsby-ssg use-data`,
-    siteUrl: ``,
+    siteUrl: `https://gatsby.dev/graphql-typegen`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -28,6 +28,16 @@ const config: GatsbyConfig = {
         path: "./src/images/",
       },
       __key: "images",
+    },
+    "gatsby-plugin-alias-imports",
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          "@": "src",
+        },
+        extensions: ["js", "jsx", "ts", "tsx"],
+      },
     },
   ],
 };
