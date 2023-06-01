d9fba29a0de2188755b275b55bb58b614c036136
ayakamada <sdkamada@gmail.com>
2023-06-01 17:49:28 +0900

    Create Ipost.ts

---
+++ use-data/src/types/Ipost.ts
@@ -0,0 +1,11 @@
+export interface IPost {
+  id: string;
+  title: string;
+  slug: string;
+  description: string;
+  date: number;
+  createdAt: string;
+  updatedAt: string;
+  published: boolean;
+  path: string;
+}
